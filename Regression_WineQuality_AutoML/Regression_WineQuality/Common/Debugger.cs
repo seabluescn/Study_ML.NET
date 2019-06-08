cabadb3b57ade9db7dcce9938f20d0c89a731bd5
GaoHaichun <gao.haichun@ztesoft.com>
2019-06-08 15:01:17 +0800

    autoML

---
+++ Regression_WineQuality_AutoML/Regression_WineQuality/Common/Debugger.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Regression_WineQuality.Common
+{
+    public class RegressionExperimentProgressHandler : IProgress<RunDetail<RegressionMetrics>>
+    {
+        private int _iterationIndex;
+
+        public void Report(RunDetail<RegressionMetrics> iterationResult)
+        {
+            _iterationIndex++;
+            Console.WriteLine($"Report index:{_iterationIndex},TrainerName:{iterationResult.TrainerName},RuntimeInSeconds:{iterationResult.RuntimeInSeconds}");
+            
+            if (iterationResult.Exception != null)
+            {
+                Console.WriteLine($"Exception during AutoML iteration: {iterationResult.Exception}");
+            }
+        }
+    }
+
+    public class Debugger
+    {
+        private const int Width = 114;
+
+        public  static void PrintTopModels(ExperimentResult<RegressionMetrics> experimentResult)
+        {
+            // Get top few runs ranked by R-Squared.
+            // R-Squared is a metric to maximize, so OrderByDescending() is correct.
+            // For RMSE and other regression metrics, OrderByAscending() is correct.
+            var topRuns = experimentResult.RunDetails
+                .Where(r => r.ValidationMetrics != null && !double.IsNaN(r.ValidationMetrics.RSquared))
+                .OrderByDescending(r => r.ValidationMetrics.RSquared);
+
+            Console.WriteLine("Top models ranked by R-Squared --");
+            PrintRegressionMetricsHeader();
+            for (var i = 0; i < topRuns.Count(); i++)
+            {
+                var run = topRuns.ElementAt(i);
+                PrintIterationMetrics(i + 1, run.TrainerName, run.ValidationMetrics, run.RuntimeInSeconds);
+            }
+        }
+
+       
+
+        public static void PrintRegressionMetricsHeader()
+        {
+            CreateRow($"{"",-4} {"Trainer",-35} {"RSquared",8} {"Absolute-loss",13} {"Squared-loss",12} {"RMS-loss",8} {"Duration",9}", Width);
+        }
+
+        public static void PrintIterationMetrics(int iteration, string trainerName, RegressionMetrics metrics, double? runtimeInSeconds)
+        {
+            CreateRow($"{iteration,-4} {trainerName,-35} {metrics?.RSquared ?? double.NaN,8:F4} {metrics?.MeanAbsoluteError ?? double.NaN,13:F2} {metrics?.MeanSquaredError ?? double.NaN,12:F2} {metrics?.RootMeanSquaredError ?? double.NaN,8:F2} {runtimeInSeconds.Value,9:F1}", Width);
+        }
+
+        public static void CreateRow(string message, int width)
+        {
+            Console.WriteLine("|" + message.PadRight(width - 2) + "|");
+        }
+
+
+        public static void PrintRegressionMetrics(string name, RegressionMetrics metrics)
+        {
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Metrics for {name} regression model      ");
+            Console.WriteLine($"*------------------------------------------------");
+            Console.WriteLine($"*       LossFn:        {metrics.LossFunction:0.##}");
+            Console.WriteLine($"*       R2 Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine($"*       Absolute loss: {metrics.MeanAbsoluteError:#.##}");
+            Console.WriteLine($"*       Squared loss:  {metrics.MeanSquaredError:#.##}");
+            Console.WriteLine($"*       RMS loss:      {metrics.RootMeanSquaredError:#.##}");
+            Console.WriteLine($"*************************************************");
+        }
+    }
+}
---
+++ Regression_WineQuality_AutoML/Regression_WineQuality/Program.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+using Regression_WineQuality.Common;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Regression_WineQuality
+{
+    public class WineData
+    {
+        [LoadColumn(0)]
+        public float FixedAcidity;
+
+        [LoadColumn(1)]
+        public float VolatileAcidity;
+
+        [LoadColumn(2)]
+        public float CitricACID;
+
+        [LoadColumn(3)]
+        public float ResidualSugar;
+
+        [LoadColumn(4)]
+        public float Chlorides;
+
+        [LoadColumn(5)]
+        public float FreeSulfurDioxide;
+
+        [LoadColumn(6)]
+        public float TotalSulfurDioxide;
+
+        [LoadColumn(7)]
+        public float Density;
+
+        [LoadColumn(8)]
+        public float PH;
+
+        [LoadColumn(9)]
+        public float Sulphates;
+
+        [LoadColumn(10)]
+        public float Alcohol;
+      
+        [LoadColumn(11)]
+        [ColumnName("Label")]
+        public float Quality;
+
+        [LoadColumn(12)]       
+        public float ID; 
+    }
+
+    public class WinePrediction
+    {
+        [ColumnName("Score")]
+        public float PredictionQuality;
+    }
+ 
+
+    class Program
+    {
+        static readonly string ModelFilePath = Path.Combine(Environment.CurrentDirectory, "MLModel", "model.zip");
+        static readonly string TrainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "winequality-data-train.csv");
+        static readonly string TestDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "winequality-data-test.csv");
+
+        static void Main(string[] args)
+        {           
+            TrainAndSave();
+            LoadAndPrediction();
+
+            Console.WriteLine("Hit any key to finish the app");
+            Console.ReadKey();
+        }
+
+        public static void TrainAndSave()
+        {
+            MLContext mlContext = new MLContext(seed: 1);
+
+            // 准备数据 
+            var trainData = mlContext.Data.LoadFromTextFile<WineData>(path: TrainDataPath, separatorChar: ',', hasHeader: true);
+            var testData = mlContext.Data.LoadFromTextFile<WineData>(path: TestDataPath, separatorChar: ',', hasHeader: true);
+         
+            var progressHandler = new RegressionExperimentProgressHandler();
+            uint ExperimentTime = 200;
+
+            ExperimentResult<RegressionMetrics> experimentResult = mlContext.Auto()
+               .CreateRegressionExperiment(ExperimentTime)
+               .Execute(trainData, "Label", progressHandler: progressHandler);           
+
+            Debugger.PrintTopModels(experimentResult);
+
+            RunDetail<RegressionMetrics> best = experimentResult.BestRun;
+            ITransformer trainedModel = best.Model;
+
+            // 评估 BestRun
+            var predictions = trainedModel.Transform(testData);
+            var metrics = mlContext.Regression.Evaluate(predictions, labelColumnName: "Label", scoreColumnName: "Score");
+            Debugger.PrintRegressionMetrics(best.TrainerName, metrics);
+
+            // 保存模型
+            Console.WriteLine("====== Save model to local file =========");
+            mlContext.Model.Save(trainedModel, trainData.Schema, ModelFilePath);           
+        }
+       
+
+        static void LoadAndPrediction()
+        {
+            MLContext mlContext = new MLContext(seed: 1);
+
+            ITransformer loadedModel = mlContext.Model.Load(ModelFilePath, out var modelInputSchema);
+            var predictor = mlContext.Model.CreatePredictionEngine<WineData, WinePrediction>(loadedModel);
+
+            WineData wineData = new WineData
+            {
+                FixedAcidity = 7.6f,
+                VolatileAcidity = 0.33f,
+                CitricACID = 0.36f,
+                ResidualSugar = 2.1f,
+                Chlorides = 0.034f,
+                FreeSulfurDioxide = 26f,
+                TotalSulfurDioxide = 172f,
+                Density = 0.9944f,
+                PH = 3.42f,
+                Sulphates = 0.48f,
+                Alcohol = 10.5f
+            };
+
+            var wineQuality = predictor.Predict(wineData);
+            Console.WriteLine($"Wine Data  Quality is:{wineQuality.PredictionQuality} ");           
+        }
+    }
+}
